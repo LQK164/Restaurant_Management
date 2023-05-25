cb51f0a85bee5fdf1bf011fd501b4abb275b82f7
unknown <Acer@Client1>
2023-05-25 14:25:04 +0700

    Hai 4th commit - Client finished

---
+++ Client/Menu_login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Menu_login : Form
+    {
+        public Menu_login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_NhanVien_Click(object sender, EventArgs e)
+        {
+            Client_NV NV = new Client_NV();
+            NV.ShowDialog();
+        }
+
+        private void btn_QuanLy_Click(object sender, EventArgs e)
+        {
+            Client_QL QL = new Client_QL();
+            QL.ShowDialog();
+        }
+    }
+}
